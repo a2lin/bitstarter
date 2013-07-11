3b24c5228c900e471b2859db8f5df2928c5008dc
a2lin <lin.alexander.2011@gmail.com>
2013-07-11 04:57:44 +0000

    reading index.html from disk

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var bufToPrint = fs.readFileSync('index.html');
+var textToPrint = bufToPrint.toString(); 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(textToPrint);
 });
 
 var port = process.env.PORT || 5000;
